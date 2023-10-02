28a2ab6c74b39372e5115e2402cc3cbb8b22b77c
leodor-tom <eleodoritom@gmail.com>
2023-10-02 15:53:43 +0200

    second exercise

---
+++ src/Exercise2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args){
+        Scanner input =new Scanner(System.in);
+        System.out.println("benvenuto, dopo ogni inserimento premi invio.");
+        System.out.println("inserisci il tuo saluto preferito");
+        String saluto = input.nextLine();
+        System.out.println("inserisci il tuo nome");
+        String nome = input.nextLine();
+        System.out.println("inserisci il tuo cognome");
+        String cognome = input.nextLine();
+        System.out.println(saluto + " " + nome + " " + cognome + "!");
+        input.close();
+    }
+}
